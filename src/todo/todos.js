7207096afde77fea25da289dfe4b00bd29344b7e
Artur Chyży <artur.chyzy@gmail.com>
2017-11-28 15:05:02 +0100

    Show todos count - functional component.

--- src/todo/todos.js
+++ src/todo/todos.js
@@ -1,33 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Todos extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            todos: props.todos
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Your todos for today</h1>
-                <div data-todos-count>
-                    You have {this.state.todos.length} todos!
-                </div>
-            </div>
-        );
-    }
+function Todos(props) {
+    return (
+        <div><h1>Your todos for today</h1>
+            <div data-todos-count>You have {props.todos.length} todos!</div>
+        </div>
+    )
 }
 
-Todos.defaultProps = {
-    todos: [
-        { 'id': 1, 'title': 'Clean up the fridge', 'completed': false },
-        { 'id': 2, 'title': 'quis ut nam facilis et officia qui', 'completed': false },
-        { 'id': 3, 'title': 'fugiat veniam minus', 'completed': false },
-    ]
-};
+Todos.defaultProps = { todos: [] };
 
 export default Todos;
