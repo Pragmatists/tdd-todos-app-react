9e209b5d16ef89b657dbfe5a98091fc580fda8d4
Artur Chyży <artur.chyzy@gmail.com>
2017-11-29 16:54:40 +0100

    List and new using react router

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Todos from './todo/todos';
+
+const Routes = (props) => (
+  <BrowserRouter>
+    <Switch>
+      <Redirect exact from="/" to="/home" />
+      <Route exact path="/home" component={Todos} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
