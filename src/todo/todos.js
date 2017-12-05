460cfd8334cd499ecf94e17746be84d76f34f65f
Artur Chyży <artur.chyzy@gmail.com>
2017-12-05 20:05:40 +0100

    TodoNew with Todos a component - works now

--- src/todo/todos.js
+++ src/todo/todos.js
@@ -2,15 +2,33 @@ import React from 'react';
 import TodosList from "./todosList";
 import TodosNew from "./todosNew";
 
-function Todos(props) {
-    return (
-        <div className={'col-md-offset-2'}>
-            <h1 data-todos-header>Your todos for today</h1>
-            <div data-todos-count>You have {props.todos.length} todos!</div>
-            <TodosNew onNewTodo={(todo) =>props.todos.push(todo)}/>
-            <TodosList todos={props.todos}/>
-        </div>
-    )
+class Todos extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: this.props.todos
+        }
+    }
+
+    onNewTodo = (todo) => {
+      this.setState({
+          todos: [...this.state.todos, todo]
+      })  ;
+    };
+
+    render() {
+        return (
+            <div className={'col-md-offset-2'}>
+                <h1 data-todos-header>Your todos for today</h1>
+                <div data-todos-count>You have {this.state.todos.length} todos!</div>
+                <TodosNew onNewTodo={this.onNewTodo}/>
+                <TodosList todos={this.state.todos}/>
+            </div>
+        )
+    }
+
+
 }
 
 Todos.defaultProps = { todos: [] };
